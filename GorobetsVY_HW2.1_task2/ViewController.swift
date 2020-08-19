2445390077ce21a3d932f1b20e91de80130a53bf
VGO <vgorobets@yandex.ru>
2020-08-19 16:53:27 +0300

    Initial Commit

---
+++ GorobetsVY_HW2.1_task2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GorobetsVY_HW2.1_task2
+//
+//  Created by VICTOR on 19.08.2020.
+//  Copyright © 2020 VICTOR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
