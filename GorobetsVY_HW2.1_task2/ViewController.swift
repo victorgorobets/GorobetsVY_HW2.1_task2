1d2ff2521cc91aa4d0dc7820f7e56697a6c058d6
VGO <vgorobets@yandex.ru>
2020-08-19 20:38:21 +0300

    HW 2

--- GorobetsVY_HW2.1_task2/ViewController.swift
+++ GorobetsVY_HW2.1_task2/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redLightCircle: UIView!
+    @IBOutlet weak var yellowLightCircle: UIView!
+    @IBOutlet weak var greenLightCircle: UIView!
+    @IBOutlet weak var buttonChangeLight: UIButton!
+    
+    var currentLight = "no"
+    let activeLight: CGFloat = 1.0
+    let inactiveLight: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightCircle.layer.cornerRadius = yellowLightCircle.frame.size.width / 2
+        yellowLightCircle.layer.cornerRadius = yellowLightCircle.frame.size.width / 2
+        greenLightCircle.layer.cornerRadius = greenLightCircle.frame.size.width / 2
+        
+        redLightCircle.alpha = inactiveLight
+        yellowLightCircle.alpha = inactiveLight
+        greenLightCircle.alpha = inactiveLight
+        
+        buttonChangeLight.layer.cornerRadius = 10
+        buttonChangeLight.setTitle("Start", for: .normal)
+        
     }
 
-
+    @IBAction func pressButtonChangeLight() {
+        switch activeLight {
+        case redLightCircle.alpha :
+            redLightCircle.alpha = inactiveLight
+            yellowLightCircle.alpha = activeLight
+        case yellowLightCircle.alpha:
+            yellowLightCircle.alpha = inactiveLight
+            greenLightCircle.alpha = activeLight
+        case greenLightCircle.alpha:
+            greenLightCircle.alpha = inactiveLight
+            buttonChangeLight.setTitle("Start", for: .normal)
+        default:
+            redLightCircle.alpha = activeLight
+            buttonChangeLight.setTitle("Next", for: .normal)
+        }
+    }
+    
 }
 
